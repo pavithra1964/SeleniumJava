a886025227bc0023d507f1753c082a55babb074b
pavithra1964 <74163958+pavithra1964@users.noreply.github.com>
2025-09-16 10:36:06 +0530

    Update NewTestGit by PK.java

--- src/withoutbrowser/NewTestGit.java
+++ src/withoutbrowser/NewTestGit.java
@@ -8,7 +8,16 @@ import org.testng.annotations.BeforeTest;
 public class NewTestGit {
 	WebDriver driver;
   @Test
-  public void f() {
+  public void techlearn() {
+	  driver.get("https://techlearn.in/admin");
+	  Thread.sleep(2000);
+		  driver.findElement(By.id("user_login")).sendKeys("Dharani");
+		  Thread.sleep(2000);
+		  driver.findElement(By.name("pwd")).sendKeys("Hello@123");
+		  Thread.sleep(2000);
+		  driver.findElement(By.id("rememberme")).click();
+		  Thread.sleep(2000);
+		  driver.findElement(By.className("wp-login-lost-password")).click();
   }
   @BeforeTest
   public void beforeTest() {
