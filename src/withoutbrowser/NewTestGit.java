06a6030812289cb9008622dfb39ad30cadced72c
pavithra1964 <74163958+pavithra1964@users.noreply.github.com>
2025-09-16 10:28:43 +0530

    Few update

--- src/withoutbrowser/NewTestGit.java
+++ src/withoutbrowser/NewTestGit.java
@@ -1,14 +1,19 @@
 package withoutbrowser;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class NewTestGit {
+	WebDriver driver;
   @Test
   public void f() {
   }
   @BeforeTest
   public void beforeTest() {
+	  driver= new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
